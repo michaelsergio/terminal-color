f092841261168b72653970a7fe0e43278bec873f
Mike Sergio <mike.sergio@manage.com>
2013-02-07 20:06:42 -0800

    initial commit

---
+++ color.js
@@ -0,0 +1,34 @@
+function colorcode(text, fgc, bgc) {
+// b r g y u m c w
+//
+// 0 - 7
+//
+
+var prop = {
+    "normal": 0,
+    "bold": 1,
+    "faint":2, 
+    "underline":4, 
+    "blink": 5,
+    "negative": 7
+};
+
+var fgp = [prop.underline, prop.bold];
+var bgp = [];
+
+var fg = ['3'+fgc].concat(fgp).join(";");
+var bg = ['4'+bgc].concat(bgp).join(";");
+
+var colors = bg ? [fg].concat(bg) : [fg];
+
+var colorings = "\\x1b[" + [fg, bg].join(";")+ 'm';
+var reset = "\\x1b[0m";
+
+var color_text = colorings + text + reset;
+return color_text;
+}
+
+function color(text,fg,bg) {
+    document.getElementById('the-code').innerText = colorcode(text,fg,bg);
+}
+
