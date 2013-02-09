43a9cee595b6f2ea9ec782a7016e7ef25c97bfde
Mike Sergio <mike.sergio@manage.com>
2013-02-08 19:27:46 -0800

    js responds to checkboxes

--- color.js
+++ color.js
@@ -14,26 +14,31 @@ var prop = {
     "negative": 7
 };
 
-
 //TODO: add properties by linking them to the checkboxes
 
 
-var fgp = [prop.underline, prop.bold];
-var bgp = [];
+var specialprops = properties.map(function(e) {
+    return prop[e];
+}).join(";");
 
-var fg = ['3'+fgc].concat(fgp).join(";");
-var bg = ['4'+bgc].concat(bgp).join(";");
+if (specialprops !== "") specialprops = ";" + specialprops;
+
+
+var fg = ['3'+fgc].join(";");
+var bg = ['4'+bgc].join(";");
 
 var colors = bg ? [fg].concat(bg) : [fg];
 
-var colorings = "\\x1b[" + [fg, bg].join(";")+ 'm';
+
+var colorings = "\\x1b[" + [fg, bg].join(";") + specialprops + 'm';
 var reset = "\\x1b[0m";
 
 var color_text = colorings + text + reset;
 return color_text;
 }
 
-function color(text,fg,bg) {
-    document.getElementById('the-code').innerText = colorcode(text,fg,bg);
+function color(text,fg,bg, opts) {
+    if (!opts) opts = [];
+    document.getElementById('the-code').innerText = colorcode(text,fg,bg, opts);
 }
 
